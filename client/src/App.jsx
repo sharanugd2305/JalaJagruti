a59be8db84c4d47d050e959cb48bbbeed7fb69fd
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-09-18 21:24:54 +0530

    feat(client) :  Update with Router in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 import { Routes ,Route} from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
 
 function App() {
-  <>
+  return (
+    <>
       <Routes>
-        {/* <Route path="/" element={<Dashboard />} /> */}
-        <Route path="/" element={<h1 className="text-3xl font-bold underline">Dashboard</h1>} />
+        <Route path="/" element={<Dashboard />} />
+        {/* <Route path="/about" element={<h1>About Page</h1>} /> */}
       </Routes>
     </>
+  )
 }
 
-export default App;
+export default App
