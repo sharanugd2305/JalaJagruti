24ef76608739682368b0cf09d86bf1d679c2e9bf
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-09-18 21:26:16 +0530

    feat(client) : updated with WaterRiskProvider context and leaftet

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import 'leaflet/dist/leaflet.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { WaterRiskProvider } from './context/WaterRiskContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <WaterRiskProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </WaterRiskProvider>
   </StrictMode>,
 )
